2e3a3b4ddc144fd8bf26c8ae6fd028fb48e9b203
John Allison <jrallison@gmail.com>
2011-07-27 12:30:40 -0400

    Some light refactoring of authenticate middleware

--- lib/sso/middleware/authenticate.rb
+++ lib/sso/middleware/authenticate.rb
@@ -8,45 +8,55 @@ module SSO
       def call(env)
         request = Rack::Request.new(env)
 
-        case request.path
-        when /^\/sso\/auth/
-          if token = SSO::Token.find(request.path.gsub("/sso/auth/", ""))
-
-            if existing_token = SSO::Token.find(request.session[:sso_token])
-              existing_token.update!(token)
-              token.destroy
-              token = existing_token
-            end
-
-            request.session[:sso_token] = token.key
-            redirect_to "http://#{token.request_domain}#{token.request_path}#{token.request_path.match(/\?/) ? "&sso=" : "?sso="}#{token.key}"
-          else
-            redirect_to request.referrer
-          end
-        else
-          if request.params['sso']
-            if request.session[:originator_key] && token = SSO::Token.find(request.params['sso'])
-              if request.session[:originator_key] == token.originator_key
-                request.session[:sso_token] = token
-                redirect_to "http://#{token.request_domain}#{token.request_path}"
-              else
-                @app.call(env)
-              end
-            else
-              @app.call(env)
-            end
-          elsif is_bot?(request) || SSO::Token.find(request.session[:sso_token])
-            @app.call(env)
-          else
-            token = SSO::Token.new
-            token.populate!(request)
-            request.session[:originator_key] = token.originator_key
-            redirect_to "http://centraldomain.com/sso/auth/#{token.key}"
-          end
+        if request.path =~ /^\/sso\/auth/
+          authenticate(request, env)
+        elsif request.params['sso']
+          verify(request, env)
+        elsif verified?(request)
+          verified?(request) ? @app.call(env) : redirect_to_central(request, env)
         end
       end
 
     private
+
+      def verified?(request)
+        is_bot?(request) || SSO::Token.find(request.session[:sso_token])
+      end
+
+      def authenticate(request, env)
+        if token = SSO::Token.find(request.path.gsub("/sso/auth/", ""))
+          if existing_token = SSO::Token.find(request.session[:sso_token])
+            existing_token.update!(token)
+            token.destroy
+            token = existing_token
+          end
+
+          request.session[:sso_token] = token.key
+          redirect_to "http://#{token.request_domain}#{token.request_path}#{token.request_path.match(/\?/) ? "&sso=" : "?sso="}#{token.key}"
+        else
+          redirect_to request.referrer
+        end
+      end
+
+      def verify(request, env)
+        if request.session[:originator_key] && token = SSO::Token.find(request.params['sso'])
+          if request.session[:originator_key] == token.originator_key
+            request.session[:sso_token] = token
+            redirect_to "http://#{token.request_domain}#{token.request_path}"
+          else
+            @app.call(env)
+          end
+        else
+          @app.call(env)
+        end
+      end
+
+      def redirect_to_central(request, env)
+        token = SSO::Token.new
+        token.populate!(request)
+        request.session[:originator_key] = token.originator_key
+        redirect_to "http://centraldomain.com/sso/auth/#{token.key}"
+      end
 
       # TODO move to a configuration option
       # checks the user agent against a list of bots
